3e8c8d591293638c1aea16f957cf90744756d111
Mickael van der Beek <mickael@sagacify.com>
2015-01-09 15:48:18 +0100

    Changed module structure.

--- src/Hydrator.js
+++ src/Hydrator.js
@@ -1,78 +1,72 @@
 module.exports = (function () {
 	'use strict';
 
-	function hydrator (res, model, hydrateOptions) {
-		var modelName = model ? model.modelName : null
-		  , hits;
-
-		if(res.hits && res.hits.hits && res.hits.hits.length && (hits = res.hits.hits)) {
-			var hit;
-			var model;
-
-			for (var i = 0; i < hits.length; i++) {
-				hit = hits[i];
-				hits[i]._source = hydrateDocument(hit, modelName, hydrateOptions);
-			}
-		}
-
-		return res;
-	}
-
-	function hydratorLean (res, model, hydrateOptions) {
-		var modelName = model ? model.modelName : null
-		  , hits;
-
-		var objectList = []
-
-		if (res.hits && res.hits.hits && res.hits.hits.length && (hits = res.hits.hits)) {
-			var hit;
-			var model;
-
-			for (var i = 0; i < hits.length; i++) {
-			 	hit = hits[i];
-				objectList.push(hydrateDocument(hit, modelName, hydrateOptions));
-			}
-		}
-
-		return objectList;
-	}
-
-	function hydrateDocument (hit, modelName, hydrateOptions) {
-		var data = hit._source
-
-		var model = getModel(hydrateOptions);
-
-		if (model === undefined) {
-			return data;
-		}
-		
-		return new model(data);
-	}
-
-	function getModel (hydrateOptions) {
-		var models = mongoose.models
-		  , model;
-
-		if (hydrateOptions && hit._type in hydrateOptions) {
-			model = hydrateOptions[hit._type];
-		}
-		
-		if (hit._type in models) {
-			model = models[hit._type];
-		}
-		
-		if (modelName in models) {
-			model = models[modelName];
-		}
-
-		return model;
-	}
-
 	return {
-		hydrateDocument: hydrateDocument,
-		hydratorLean: hydratorLean,
-		hydrator: hydrator,
+		hydrator: function (res, model, hydrateOptions) {
+			var modelName = model ? model.modelName : null
+			  , hits;
 
-		getModel: getModel
+			if(res.hits && res.hits.hits && res.hits.hits.length && (hits = res.hits.hits)) {
+				var hit;
+				var model;
+
+				for (var i = 0; i < hits.length; i++) {
+					hit = hits[i];
+					hits[i]._source = this.hydrateDocument(hit, modelName, hydrateOptions);
+				}
+			}
+
+			return res;
+		},
+
+		hydratorLean: function (res, model, hydrateOptions) {
+			var modelName = model ? model.modelName : null
+			  , hits;
+
+			var objectList = []
+
+			if (res.hits && res.hits.hits && res.hits.hits.length && (hits = res.hits.hits)) {
+				var hit;
+				var model;
+
+				for (var i = 0; i < hits.length; i++) {
+				 	hit = hits[i];
+					objectList.push(this.hydrateDocument(hit, modelName, hydrateOptions));
+				}
+			}
+
+			return objectList;
+		},
+
+		hydrateDocument: function (hit, modelName, hydrateOptions) {
+			var data = hit._source
+
+			var model = this.getModel(hydrateOptions);
+
+			if (model === undefined) {
+				return data;
+			}
+			
+			return new model(data);
+		},
+
+		getModel: function (hydrateOptions) {
+			var models = mongoose.models
+			  , model;
+
+			if (hydrateOptions && hit._type in hydrateOptions) {
+				model = hydrateOptions[hit._type];
+			}
+			
+			if (hit._type in models) {
+				model = models[hit._type];
+			}
+			
+			if (modelName in models) {
+				model = models[modelName];
+			}
+
+			return model;
+		}
 	};
 })();
